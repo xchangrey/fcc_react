671cc967d1fe87ac73d5f110f665afc4698021dc
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-23 15:48:40 -0400

    Adding the notes for shouldComponentUpdate LF

---
+++ shouldComponentUpdate.js
@@ -0,0 +1,54 @@
+//React: Optimize Re-Renders with shouldComponentUpdate
+
+// a lifecycle method you can call when child components receive new state or props, and declare specifically if the components should update or not.
+// 
+// it takes nextProps and nextState as parameters
+// 
+// this method is a useful way to optimize performance. For example, the default behavior is that your component re-renders when it receives new props, even if the props haven't changed. You can use shouldComponentUpdate() to prevent this by comparing the props. The method must return a boolean value that tells React whether or not to update the component. You can compare the current props (this.props) to the next props (nextProps) to determine if you need to update or not, and return true or false accordingly.
+// 
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+     // change code below this line
+    if (nextProps.value % 2 === 0) {
+      return true;
+    }
+     // change code above this line
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log('Receiving new props...');
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>
+  }
+};
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState({
+      value: this.state.value + 1
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value}/>
+      </div>
+    );
+  }
+};
