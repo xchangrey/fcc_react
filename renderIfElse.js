e5209cb1ea624fda79a2e7b9bcc5cec496bf8d32
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-24 15:38:06 -0400

    Adding the notes on rendering with an if/else condition

---
+++ renderIfElse.js
@@ -0,0 +1,36 @@
+// React: Render with an If/Else Condition
+// 
+// Another application of using JavaScript to control your rendered view is to tie the elements that are rendered to a condition. When the condition is true, one view renders. When it's false, it's a different view. You can do this with a standard if/else statement in the render() method of a React component.
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState({
+      display: !this.state.display
+    });
+  }
+  render() {
+    // change code below this line
+  if(this.state.display){
+      return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+  } else {
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+       </div>
+    );
+  }
+
+  }
+};
