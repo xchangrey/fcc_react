15c4fd73eab0366f32a42d6a8a8cb25fc8459bf0
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-24 17:57:23 -0400

    Adding the notes on rendering conditionally from props

---
+++ renderConditionallyFromProps.js
@@ -0,0 +1,47 @@
+//React: Render Conditionally from Props
+
+
+//Using props to conditionally render code is very common with React developers — that is, they use the value of a given prop to automatically make decisions about what to render.
+//
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>
+      {
+        this.props.fiftyFifty > 0 ? 'You lose!' : 'You win!'
+      }
+      </h1>
+    )
+  };
+};
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      counter: this.state.counter + 1 // change code here
+    });
+  }
+  render() {
+    let expression = Math.floor(Math.random() > 0.5); // change code here
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        { /* change code below this line */ }
+        <Results fiftyFifty={expression} />
+        { /* change code above this line */ }
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+};
