d1544a3c182a9f025c3e1a0859d077f9c14a7a0c
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-24 14:58:22 -0400

    Updating the inline styling on React

---
+++ inlineStyles.js
@@ -0,0 +1,10 @@
+//React: Introducing Inline Styles
+//
+//You apply inline styles to JSX elements similar to how you do it in HTML, but with a few JSX differences. Here's an example of an inline style in HTML:
+
+// <div style="color: yellow; font-size: 16px">Mellow Yellow</div>. In React, inline styles look like this: <div style={{color: "yellow", fontSize: 16}}>Mellow Yellow</div>
+//
+//You set it equal to a JS object, with the proerty in camelCase.
+//
+//Note that you can optionally set the font size to be a number, omitting the units "px", or write it as "72px".
+//
