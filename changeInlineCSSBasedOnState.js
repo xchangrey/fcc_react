6d873665ca96b4bbc052e4e860fb64ba69cdcbaf
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-25 12:28:48 -0400

    Adding the notes for changing inline CSS conditionally based on component state

---
+++ changeInlineCSSBasedOnState.js
@@ -0,0 +1,38 @@
+// React: Change Inline CSS Conditionally Based on Component State
+// 
+// To do this, you check for a condition, and if that condition is met, you modify the styles object that's assigned to the JSX elements in the render method.
+// 
+// When you set a style object based on a condition, you describe how the UI should look as a function of the application's state. There is a clear flow of information that only moves in one direction. This is the preferred method when writing applications with React.
+// 
+
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // change code below this line
+    this.state.input.length > 15 ? inputStyle.border = '3px solid red' : inputStyle
+    // change code above this line
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
