000ec8b378b759a9b6b88c62b1ad702a56a58d64
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-25 13:00:57 -0400

    Adding the notes on giving sibling elements a unique key attribute

---
+++ uniqueKeyAttr.js
@@ -0,0 +1,27 @@
+//React: Give Sibling Elements a Unique Key Attribute
+//
+//When you create an array of elements, each one needs a key attribute set to a unique value. React uses these keys to keep track of which items are added, changed, or removed. This helps make the re-rendering process more efficient when the list is modified in any way. Note that keys only need to be unique between sibling elements, they don't need to be globally unique in your application.
+//
+//
+//
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(item => <li key={frontEndFrameworks.indexOf(item)}>{item}</li>); // change code here
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
